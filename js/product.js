3c62a4bfa221ba98922ad95b316bdf5bfcc06122
a-sowa <axelsowapro@gmail.com>
2021-08-07 14:47:14 +0200

    End JS script to load products data on product.html

--- js/product.js
+++ js/product.js
@@ -20,76 +20,72 @@ fetch(apiUrl)
     .then((resp) => resp.json())
     .then(function (dataProducts) {
         let products = dataProducts;
-        let product = products.filter(product => product._id === getURLParams()) 
+        let product = products.filter(product => product._id === getURLParams())
         let main = document.querySelector('main')
         let productContainer = createNodeWithClasses('div', 'container product-container');
-        productContainer.innerHTML = 
-                `<div class="row reset-row-padding px-3">
+        productContainer.innerHTML =
+            `<div class="row reset-row-padding px-3">
                    
-                    <div class="col-12 col-lg-6 col-product-img">
-                        <img src="./img/cameras-min.jpg" class="img-fluid  product-img" alt="...">
+                <div class="col-12 col-lg-6 col-product-img">
+                    <img src="${product[0].imageUrl}" class="img-fluid  product-img" alt="...">
+                </div>
+                
+                <div class="col-12 col-lg-6 p-3 d-flex flex-column justify-content-around">
+                    
+                    <div class="product-headings">
+                        <h1 class="">${product[0].name}</h1>
+                        <div class="price ">${product[0].price}€</div>
                     </div>
                     
-                    <div class="col-12 col-lg-6 p-3 d-flex flex-column justify-content-around">
-                        
-                        <div class="product-headings">
-                            <h1 class="">${product._id}</h1>
-                            <div class="price ">00,00€</div>
-                        </div>
-                       
 
-                        <div class="product-option">
-                            <div class="quantity-selector pt-3 d-flex flex-row">
-                                <label for="product-quantity" class="mr-3">Quantité :</label>
-                                <input type="number" id="product-quantity" name="product-quantity" min="1" max="100">
-                            </div>
-
-                            <div class="option-selector  py-3">
-                                <label for="lense-choice" class="mr-3">Product option :</label>
-                                <input list="lenses" id="lense-choice" name="lense-choice" />
-
-                                <datalist id="lenses">
-                                    <option value="35mm 1.4">
-                                    <option value="50mm 1.6">
-                                </datalist>
-                            </div>
+                    <div class="product-option">
+                        <div class="quantity-selector pt-3 d-flex flex-row">
+                            <label for="product-quantity" class="mr-3">Quantité :</label>
+                            <input type="number" id="product-quantity" name="product-quantity" min="1" max="100">
                         </div>
 
-                        <a href="#" class="btn btn-add-to-cart  py-2 px-5 ">AJOUTER AU PANIER</a>
+                        <div class="option-selector  py-3">
+                            <label for="lense-choice" class="mr-3">Objectif :</label>
+                            <input list="lenses" id="lense-choice" name="lense-choice" />
+
+                            <datalist id="lenses">
+                                <option value="${product[0].lenses[0]}">
+                                <option value="${product[0].lenses[1]}">
+                            </datalist>
+                        </div>
                     </div>
+
+                    <a href="#" class="btn btn-add-to-cart  py-2 px-5 ">AJOUTER AU PANIER</a>
                 </div>
-                <div class="row px-3 pb-5">
+            </div>
+            <div class="row px-3 pb-5">
 
-                    <div class="col d-none d-md-block col-lg-6"></div>
+                <div class="col d-none d-md-block col-lg-6"></div>
 
-                    <div class=" col-12 col-lg-6 product-description pt-3">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, ad et perferendis, 
-                            rem officiis ipsum dignissimos cum veritatis
-                                vel consectetur praesentium qui, cupiditate exercitationem aliquid obcaecati quia sunt. 
-                                Assumenda, aut. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit blanditiis 
-                                ea beatae a quos iste quae error quibusdam ipsum soluta! Dolorum suscipit adipisci hic repudiandae 
-                                optio iure totam quidem voluptatibus.
-                        </p>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, ad et perferendis, 
-                            rem officiis ipsum dignissimos cum veritatis
-                                vel consectetur praesentium qui, cupiditate exercitationem aliquid obcaecati quia sunt. 
-                                Assumenda, aut. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit blanditiis 
-                                ea beatae a quos iste quae error quibusdam ipsum soluta! Dolorum suscipit adipisci hic repudiandae 
-                                optio iure totam quidem voluptatibus.
-                        </p>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, ad et perferendis, 
-                            rem officiis ipsum dignissimos cum veritatis
-                                vel consectetur praesentium qui, cupiditate exercitationem aliquid obcaecati quia sunt. 
-                                Assumenda, aut. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit blanditiis 
-                                ea beatae a quos iste quae error quibusdam ipsum soluta! Dolorum suscipit adipisci hic repudiandae 
-                                optio iure totam quidem voluptatibus.
-                        </p>  
-                    </div>
-                </div>`
-            
+                <div class=" col-12 col-lg-6 product-description pt-3">
+                    ${product[0].description}
+                    
+                    <br>
+                    <span class="d-flex flex-row justify-content-center">_____________</span>
+                    <br>
+
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    In ipsum quia numquam debitis, cum eveniet consectetur sequi 
+                    optio odio error velit iusto ullam mollitia tempore tenetur magni 
+                    consequatur fugit facere. 
+                    <br><br>
+                    
+
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    In ipsum quia numquam debitis, cum eveniet consectetur sequi 
+                    optio odio error velit iusto ullam mollitia tempore tenetur magni 
+                    consequatur fugit facere.Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    In ipsum quia numquam debitis, cum eveniet consectetur sequi 
+                    optio odio error velit iusto ullam mollitia tempore tenetur magni 
+                    consequatur fugit facere.
+                </div>
+            </div>`
+
         append(main, productContainer);
     })
     .catch(function (error) {
